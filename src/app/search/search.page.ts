5885a9bd0361d3af5fe1d5e8e5ffa6184d6e44db
hayato <hayatobasuke88@gmail.com>
2019-12-28 10:45:07 +0900

    up

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  lists: any;
+  searchQuery: '';
+  constructor() {
+    this.initializeItems();
+  }
+  initializeItems() {
+    this.lists = [
+      { name: 'カマタカフェ', relation: 'カフェ', url: '/shop' },
+      { name: 'パン屋-kamata', relation: 'パン屋', url: '/shop' },
+      { name: '蒲田屋', relation: '和菓子屋', url: '/shop' },
+    ];
+  }
+
+
+  getItems(ev: any) {
+    this.initializeItems();
+    const val = ev.target.value;
+    if (val && val.trim() !== '') {
+      this.lists = this.lists.filter((item) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    }
+  }
 
   ngOnInit() {
   }
 
+
+
 }
+
